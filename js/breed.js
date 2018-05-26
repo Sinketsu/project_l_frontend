34ab3d82d1172c2b33e895fce3411b67cee325d6
Sinketsu <ssolloss@yandex.ru>
2018-05-26 12:52:04 +0300

    work on breed page

--- js/breed.js
+++ js/breed.js
@@ -8,7 +8,7 @@ function mergefoo() {
   var f= document.getElementsByClassName("fading");
   var speed = window.innerWidth/5;
   var distance=$("#liz3").offset().left - $("#liz1").offset().left;
-  var mergetime=distance/speed;
+  var mergetime = distance / speed;
   var timer=setInterval(function(){
     var timePassed=Date.now()-start;
     liz1.style.left= speed*timePassed/1000+'px';
@@ -16,35 +16,34 @@ function mergefoo() {
     for (var i = 0; i < f.length; i++) {
       f[i].style.opacity= 1-1.5*(timePassed/1000)/mergetime;
     }
-if ($("#liz1").offset().left- $("#liz3").offset().left>0) clearInterval(timer);
-}, 20);
+    if ($("#liz1").offset().left- $("#liz3").offset().left>0)
+      clearInterval(timer);
+  }, 20);
 
-function chngvis() {
-  liz3.style.visibility='visible';
-  var timer1=setInterval(function(){
-    liz3.style.opacity=$('#liz3').css("opacity")-(-0.01);
-    if ($('#liz1').css("opacity")=='1') clearInterval(timer1);
-  }, 20)
-  document.getElementById("liz3").classList.add("col-md-12");
-  var zoom=1; //зум новой ящерки, хз, хорошая ли практика делать это вот так
-  getw.style.width=$("#setw1").outerWidth()*zoom+'px';
-  liz2.style.visibility='hidden';
-  liz1.style.visibility='hidden';
-  setw1.style.width='0px';
-  setw2.style.width='0px';
-  document.getElementById("setw1").classList.remove("col");
-  document.getElementById("setw2").classList.remove("col");
-  document.getElementById("btnbreed").classList.remove("col-md-12");
-  btnbreed.style.width='0px';
-  var c= document.getElementsByClassName("congrats");
-  for (var i = 0; i < c.length; i++) {
-    c[i].classList.add("col");
-    c[i].style.visibility='visible';
+  function chngvis() {
+    liz3.style.visibility='visible';
+    var timer1=setInterval(function(){
+      liz3.style.opacity=$('#liz3').css("opacity")-(-0.01);
+      if ($('#liz1').css("opacity")=='1')
+        clearInterval(timer1);
+    }, 20);
+    document.getElementById("liz3").classList.add("col-md-12");
+    var zoom=1; //зум новой ящерки, хз, хорошая ли практика делать это вот так
+    getw.style.width=$("#setw1").outerWidth()*zoom+'px';
+    liz2.style.visibility='hidden';
+    liz1.style.visibility='hidden';
+    setw1.style.width='0px';
+    setw2.style.width='0px';
+    document.getElementById("setw1").classList.remove("col");
+    document.getElementById("setw2").classList.remove("col");
+    document.getElementById("btnbreed").classList.remove("col-md-12");
+    btnbreed.style.width='0px';
+    var c= document.getElementsByClassName("congrats");
+    for (var i = 0; i < c.length; i++) {
+      c[i].classList.add("col");
+      c[i].style.visibility='visible';
+    }
   }
-
-
-
-}
  setTimeout(chngvis, 0.8*mergetime*1000);
 
-};
+}
